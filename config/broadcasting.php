70e25bc6aacb71018d640006e8e65f80e9005d24
mbosnic96 <mehmed_bossnic@hotmail.com>
2025-03-11 18:14:04 +0100

    fixed broadcast, needs fix on nulls on alerts

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,13 +40,8 @@ return [
              //   'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
              //   'port' => env('PUSHER_PORT', 443),
             //    'scheme' => env('PUSHER_SCHEME', 'https'),
-           //     'encrypted' => true,
+                'encrypted' => true,
              //   'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
-             'useTLS' => true,
-             'curl_options' => [
-                CURLOPT_SSL_VERIFYPEER => false, // Disable SSL verification
-                CURLOPT_SSL_VERIFYHOST => false, // Disable host verification
-             ],
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
